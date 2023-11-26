a5e8b0b216f508479a171540508b05a1c391252b
ginmus <wjq20010505@163.com>
2023-11-26 17:56:21 +0800

    完成monorepo

---
+++ lib/src/main.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: ginmus wjq20010505@163.com
+ * @Date: 2023-11-26 17:49:01
+ * @LastEditors: ginmus wjq20010505@163.com
+ * @LastEditTime: 2023-11-26 17:49:22
+ * @FilePath: \Editor\lib\src\main.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+const text = 'demo';
+
+export default text;
